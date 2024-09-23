57089dfbc92b34d33dacd151df3e0d9df217bcab
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-09-23 20:12:49 +0300

    Added closing functionality to the full page project

--- JavaScript/FullPage.js
+++ JavaScript/FullPage.js
@@ -7,15 +7,11 @@ let overlay2 = document.querySelector('.overlay2')
 let overlay = document.getElementById('myNav')
 let overlay3 = document.querySelector('.overlay3')
 
-let closeBtn = document.querySelector('.open')
-
-sideBtn.addEventListener('click', () => {
-    overlay.style.width = '100%'
-})
+let closeBtn = document.querySelectorAll('.open')
 
 // Opens from the side
-closeBtn.addEventListener('click', () => {
-    overlay.style.width = '0'
+sideBtn.addEventListener('click', () => {
+    overlay.style.width = '100%'
 })
 
 // Opens from the top
@@ -28,3 +24,12 @@ topBtn.addEventListener('click', () => {
 instant.addEventListener('click', () => {
     overlay3.style.display = 'block'
 })
+
+// Closing the menu
+for(let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener('click', () => {
+        overlay.style.width = '0'
+        overlay2.style.height = '0'
+        overlay3.style.display = 'none'
+    }
+)}
